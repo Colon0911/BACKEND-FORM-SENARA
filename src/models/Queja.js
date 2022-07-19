be1ef69c3a770a79d0d89d73cca1d6709a27ad64
abiel <abieljosue1@gmail.com>
2022-07-19 15:58:59 -0600

    Quejas v1

---
+++ src/models/Queja.js
@@ -0,0 +1,87 @@
+import mongoose from 'mongoose'
+
+
+
+const { model, Schema } = mongoose
+
+const quejaSchema = new Schema({
+    userID: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    dateNow: {
+        type: Date,
+        required: true,
+        trim: true
+    },
+    hourNow: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    tipoUsuario: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    telefono: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lugar: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    nParcela: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    nToma: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    problematica: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    reportado: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    respInst: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    solucion: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    aporte: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    nombreQuejoso: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    cedula: {
+        type: String,
+        required: true,
+        trim: true
+    }
+}, {
+    versionKey: false
+})
+
+export default model("Queja", quejaSchema)
