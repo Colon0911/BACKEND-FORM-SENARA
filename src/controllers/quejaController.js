d4f59d0129ec1ed1817c24063265dccdd0a49ed8
abiel <abieljosue1@gmail.com>
2022-07-20 16:50:11 -0600

    Update controlador quejas

--- src/controllers/quejaController.js
+++ src/controllers/quejaController.js
@@ -1,17 +1,20 @@
 import Queja from "../models/Queja.js"
-
+import User from "../models/User.js";
 
 export const agregarQueja = async (req, res) => {
-    console.log("...")
     try {
         if (!req.body) {
             return res.status(401).json({ msg: "Información no suministrada!" })
         }
+        const data = req.body
+        const user = await User.find({ identification: data.identification })
+        delete data.identification
+        data["userID"] = user[0]._id
         const newQueja = new Queja(req.body)
-        console.log(newQueja)
-        // await newQueja.save()
+        await newQueja.save()
         return res.status(200).json({ msg: "Queja registrada con exito!" })
     } catch (error) {
+        console.log(error)
         return res.status(500).json({ msg: "Error Inesperado!" })
     }
 }
